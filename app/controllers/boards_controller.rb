2ff198cd29dc087b0feb20074966ea6ce09ea75d
nodarby <ndtmhr@outlook.jp>
2019-11-30 23:13:20 +0900

    新規トピック追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -8,4 +8,33 @@ class BoardsController < ApplicationController
     @msg = "Create"
     @topic = Topic.new
   end
+
+  def create
+    @topic = Topic.new(topic_params)
+
+    respond_to do |format|
+      if @topic.save
+        format.html { redirect_to({action: :show, id: @topic.id}, notice: 'Topic was successfully created.')}
+        format.json { render :show, status: :created, location: @topic}
+      else
+        format.html {render :new}
+        format.json {render json: @topic.errors, status: :unprocessable_entity}
+      end
+    end
+  end
+
+  before_action :set_topic, only: [:show]
+
+  def show
+
+  end
+
+  private
+    def set_topic
+      @topic = Topic.find(params[:id])
+    end
+
+    def topic_params
+      params.require(:topic).permit(:title)
+    end
 end
